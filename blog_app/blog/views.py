fb4bd74a9fb8fc955d8dfd6b4d17c7fd45409c4e
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-07 21:54:31 -0500

    create templates for home and about pages

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+post = [
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "09/08/2009"
+    },
+        {
+        "author": "John Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "09/18/2009"
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': post
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
