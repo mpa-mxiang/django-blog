2f579fbd50c8a4a46cfaab47b7ee98dec8964deb
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-21 23:11:06 -0500

    delete hardcoded post data and format date

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-post = [
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "09/08/2009"
-    },
-        {
-        "author": "John Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "09/18/2009"
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
